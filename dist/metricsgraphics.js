2cf62e48c38461b87ed73a4a94eb78ce27a6be0e
Zeender, Robert <robert.zeender@experient-inc.com>
2016-07-08 16:17:46 -0400

    Remove jQuery import.

--- dist/metricsgraphics.js
+++ dist/metricsgraphics.js
@@ -1,12 +1,12 @@
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
-    define(['d3', 'jquery'], factory);
+    define(['d3'], factory);
   } else if (typeof exports === 'object') {
-    module.exports = factory(require('d3'), require('jquery'));
+    module.exports = factory(require('d3'));
   } else {
-    root.MG = factory(root.d3, root.jQuery);
+    root.MG = factory(root.d3);
   }
-}(this, function(d3, $) {
+}(this, function(d3) {
 window.MG = {version: '2.8.0'};
 
 function register(chartType, descriptor, defaults) {
